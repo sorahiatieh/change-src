c497faea21249dffd7064d8c989815c910ddf028
atieh <a.sorahitalab@gmail.com>
2023-12-08 19:24:39 +0330

    change src

---
+++ js/script.js
@@ -0,0 +1,17 @@
+﻿
+let bulbImg=document.getElementById('bulb');
+let btn=document.getElementById('btn');
+
+let bulbFlag=false;
+
+function turnOn(){
+    if(bulbFlag)    {
+        bulbImg.setAttribute('src','pics/bulboff.gif');
+        btn.innerHTML='Turn On';
+        bulbFlag=false;
+    }else{
+        bulbImg.setAttribute('src','pics/bulbon.gif');
+        btn.innerHTML='Turn Off';
+        bulbFlag=true;
+    }
+}
